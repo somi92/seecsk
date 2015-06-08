d7f7ecb402377157b96859edb8b159119bc3593d
Milos Stojanovic <milos92s@yahoo.com>
2015-06-08 14:17:57 +0200

    Refactoring.

--- src/com/github/somi92/seecsk/main/Main.java
+++ src/com/github/somi92/seecsk/main/Main.java
@@ -6,6 +6,8 @@
 package com.github.somi92.seecsk.main;
 
 import com.github.somi92.seecsk.gui.FMainForm;
+import com.github.somi92.seecsk.util.Config;
+import com.github.somi92.seecsk.util.Constants;
 import com.github.somi92.sqldbb.broker.DBBroker;
 
 /**
@@ -17,7 +19,14 @@ public class Main {
     public static void main(String[] args) {
         
         DBBroker broker = new DBBroker();
-        broker.setDatabase("seecsk");
+        broker.setDatabaseServer(Config.vratiInstancu().vratiVrednost(Constants.DatabaseConfigKeys.DB_SERVER));
+        
+        broker.setDatabaseDriver(Config.vratiInstancu().vratiVrednost(Constants.DatabaseConfigKeys.DB_DRIVER));
+        broker.setHostPort(Config.vratiInstancu().vratiVrednost(Constants.DatabaseConfigKeys.DB_HOST)+":"+
+                Config.vratiInstancu().vratiVrednost(Constants.DatabaseConfigKeys.DB_PORT));
+        broker.setDatabase(Config.vratiInstancu().vratiVrednost(Constants.DatabaseConfigKeys.DB_NAME));
+        broker.setUsername(Config.vratiInstancu().vratiVrednost(Constants.DatabaseConfigKeys.DB_USER));
+        broker.setPassword(Config.vratiInstancu().vratiVrednost(Constants.DatabaseConfigKeys.DB_PASSWORD));
         
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
